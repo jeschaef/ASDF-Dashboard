fd3d29694b7503de0fa30c3a8fe89f800d7fb9ef
Jero <jeros.2808@gmail.com>
2022-05-16 08:10:37 +0200

    Uses redis cache now

--- app/cache.py
+++ app/cache.py
@@ -1,6 +1,10 @@
 from flask_caching import Cache
 
 cache = Cache(config={
-    'CACHE_TYPE': 'SimpleCache'
+    # 'CACHE_TYPE': 'SimpleCache'
+    'CACHE_TYPE': 'RedisCache',
+    'CACHE_REDIS_HOST': 'localhost',
+    'CACHE_REDIS_PORT': 6379,
+    'CACHE_KEY_PREFIX': 'cache'         # prevent flushing of whole db on cache clear (e.g. celery queue)
 })
 
