6b395dc7e42f34d1d910dd44d5e42507da947835
Jero <jeros.2808@gmail.com>
2022-06-03 09:03:13 +0200

    Layout & formatting update

--- app/static/js/datasets.js
+++ app/static/js/datasets.js
@@ -4,9 +4,6 @@ let $remove = $("#remove")
 window.operateEvents = {
     // Click inspect operation
     'click .inspect': function (e, value, row, index) {
-        console.log('Clicked row: ' + JSON.stringify(row))
-        console.log('Row name: ' + row.name)
-
         // Add hidden form with dataset names and submit it (redirects)
         const url = $table.attr("url-inspect")
         let form = $('<form action="' + url + '" method="POST"></form>');
@@ -19,6 +16,7 @@ window.operateEvents = {
         form.submit();
     }
 }
+
 
 function operationFormatter(value, row, index, field) {
     let element = $('<a class="inspect" href="javascript:void(0)" title="Inspect"></a>')
@@ -33,18 +31,23 @@ function getNameSelections() {
     })
 }
 
+
+function dateFormatter(value) {
+    // Format date based on default locale
+    const date = new Date(value)
+    return date.toLocaleString([], {dateStyle: "long", timeStyle: "short"})
+}
+
+
 function initTable() {
 
     $table.bootstrapTable()
     $table.on('check.bs.table uncheck.bs.table check-all.bs.table uncheck-all.bs.table',
         function () {
-            // En-/disable delete button
+            // En-/disable delete button based on whether at least one datasets is selected or not
             $remove.prop('disabled', !$table.bootstrapTable('getSelections').length)
         })
 
-    // $table.on('all.bs.table', function (e, name, args) {
-    //     console.log(name, args)
-    // })
 
     $remove.click(function () {
         var names = getNameSelections()
