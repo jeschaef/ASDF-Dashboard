8b211aadbf26709e138148b61c37de4b3168e1ba
jero77 <34156935+jero77@users.noreply.github.com>
2022-06-20 15:01:01 +0200

    Fix email sender

--- app/mail.py
+++ app/mail.py
@@ -8,7 +8,7 @@ def send_confirmation_mail(name, recipient, confirmation_url):
     body = render_template("mail/confirmation.html", name=name, confirmation_url=confirmation_url)
     msg = EmailMessage(subject="Confirm your registration",
                         body=body,
-                        from_email="noreply@example.com",
+                        from_email="noreply@asdf-dashboard.com",
                         to=[recipient])
 
     msg.content_subtype = 'html'
