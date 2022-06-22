6447a9223b7e709416f3ab926ed23ec11b6ac97b
Jero <jeros.2808@gmail.com>
2022-06-22 11:05:02 +0200

    Embed logo email

--- app/mail.py
+++ app/mail.py
@@ -17,7 +17,7 @@ def send_confirmation_mail(app, name, recipient, confirmation_url):
     msg.content_subtype = 'html'
 
     # Embed logo
-    file_path = get_project_root() / "app/static/logo.png"
+    file_path = get_project_root() / "static/logo.png"
     with app.open_resource(file_path) as res:
         msg.attach(filename="logo.png", content_type="image/png", data=res.read(),
                     disposition="inline", headers=[['Content-ID', '<logo_png>']])
