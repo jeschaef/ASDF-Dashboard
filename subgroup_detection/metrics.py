73033f36974738b2ac9c0b8799eff4a781c8992b
jero77 <34156935+jero77@users.noreply.github.com>
2022-07-21 14:47:28 +0200

    Fix metrics division by 0

--- subgroup_detection/metrics.py
+++ subgroup_detection/metrics.py
@@ -1,6 +1,7 @@
 import operator
 from functools import reduce
 
+import pandas as pd
 from sklearn.metrics import confusion_matrix
 
 
@@ -11,9 +12,7 @@ def statistical_parity_difference(conf_matrix):
     :return: Statistical parity difference.
     """
     tn, fp, fn, tp, tn2, fp2, fn2, tp2 = conf_matrix
-    acc_rate_priv = (tp + fp) / (tp + fp + fn + tn)
-    acc_rate_unpriv = (tp2 + fp2) / (tp2 + fp2 + fn2 + tn2)
-    return acc_rate_unpriv - acc_rate_priv
+    return acceptance_rate(tn2, fp2, fn2, tp2) - acceptance_rate(tn, fp, fn, tp)
 
 
 def equal_opportunity_difference(conf_matrix):
@@ -22,10 +21,8 @@ def equal_opportunity_difference(conf_matrix):
     :param conf_matrix: Confusion matrices of the privileged and unprivileged subgroup.
     :return: Equal opportunity difference.
     """
-    tn, fp, fn, tp, tn2, fp2, fn2, tp2 = conf_matrix
-    tpr_priv = tp / (tp + fn)
-    tpr_unpriv = tp2 / (tp2 + fn2)
-    return tpr_unpriv - tpr_priv
+    _, _, fn, tp, _, _, fn2, tp2 = conf_matrix
+    return  recall(fn2, tp2) - recall(fn, tp)
 
 
 def average_odds_difference(conf_matrix):
@@ -35,17 +32,48 @@ def average_odds_difference(conf_matrix):
     :return: Average odds difference.
     """
     tn, fp, fn, tp, tn2, fp2, fn2, tp2 = conf_matrix
-    fpr_priv = fp / (fp + tn)
-    fpr_unpriv = fp2 / (fp2 + tn2)
-    fpr_diff = fpr_unpriv - fpr_priv
-
-    tpr_priv = tp / (tp + fn)
-    tpr_unpriv = tp2 / (tp2 + fn2)
-    tpr_diff = tpr_unpriv - tpr_priv
+    fpr_diff = specificity(tn2, fp2) - specificity(tn, fp)
+    tpr_diff = recall(fn2, tp2) - recall(fn, tp)
     return (tpr_diff + fpr_diff) / 2
 
 
-def print_metrics(cluster_rules, data, pos_label=1):
+def acceptance_rate(tn, fp, fn, tp):
+    """
+    Acceptance rate P / (P + N).
+    :param tn: True negatives.
+    :param fp: False positives.
+    :param fn: False negatives.
+    :param tp: True positives.
+    :return: Acceptance rate.
+    """
+    return (tp + fp) / (tp + fp + fn + tn)
+
+
+def recall(fn, tp):
+    """
+    Recall score tp / (tp + fn).
+    :param fn: False negatives.
+    :param tp: True positives.
+    :return: Recall score.
+    """
+    if tp + fn > 0:
+        return tp / (tp + fn)
+    return 0
+
+
+def specificity(tn, fp):
+    """
+    Specificity score fp / (fp + tn)
+    :param tn: True negatives
+    :param fp: False positives.
+    :return: Specificity score.
+    """
+    if fp + tn > 0:
+        return fp / (fp + tn)
+    return 0
+
+
+def compute_metrics(cluster_rules, data, pos_label=1):
     """
     Print statistical parity, eq. opportunity and avg. odds difference for the given rules of
     a clustering.
@@ -53,15 +81,15 @@ def print_metrics(cluster_rules, data, pos_label=1):
     :param data: The original dataset (not preprocessed) incl. ground-truth ('class')
     and predicted ('out') labels.
     :param pos_label: Label of the positive class (0 or 1).
+    :return: Dataframe with one row for each cluster (pattern)
     """
+    res = pd.DataFrame(columns=['Stat. parity diff.', 'Eq. opportunity diff.', 'Avg. odds diff.'])
     for c in cluster_rules:
         rules = cluster_rules[c]
         C = conf_matrix(data, rules, pos_label=pos_label)
+        res.loc[c] = [statistical_parity_difference(C), equal_opportunity_difference(C), average_odds_difference(C)]
+    return res
 
-        print("Cluster", c)
-        print('Stat. parity diff.\t', statistical_parity_difference(C))
-        print('Eq. opportunity diff.\t', equal_opportunity_difference(C))
-        print('Avg. odds diff.\t\t', average_odds_difference(C))
 
 def rules_to_index(data, rules):
     """
@@ -98,7 +126,6 @@ def conf_matrix(data, rules, pos_label=1):
     y_true_rest = y_true[~idx]
     y_pred_rest = y_pred[~idx]
 
-    print(y_true_group.shape, y_true_rest.shape)
     if pos_label == 1:
         tn, fp, fn, tp = confusion_matrix(y_true_group, y_pred_group, labels=[0, 1]).ravel()
         tn2, fp2, fn2, tp2 = confusion_matrix(y_true_rest, y_pred_rest, labels=[0, 1]).ravel()
