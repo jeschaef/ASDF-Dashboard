1686503f83fcac16ee77fd166e4b2de9db7000c5
jero77 <34156935+jero77@users.noreply.github.com>
2022-08-10 16:20:50 +0200

    Pattern evaluation metrics

--- subgroup_detection/metrics.py
+++ subgroup_detection/metrics.py
@@ -5,34 +5,45 @@ import numpy as np
 import pandas as pd
 from sklearn.metrics import confusion_matrix
 
+from subgroup_detection.util import _dict_avg, _dict_map, _dict_min, _dict_max
 
-def statistical_parity_difference(conf_matrix):
+
+def statistical_parity_difference(C):
     """
     Compute the statistical parity difference for the given confusion matrices.
-    :param conf_matrix: Confusion matrices of the privileged and unprivileged subgroup.
+
+    :param (int, int, int, int, int, int, int, int) C: Confusion matrices of the
+        privileged and unprivileged subgroup.
     :return: Statistical parity difference.
+    :rtype: float
     """
-    tn, fp, fn, tp, tn2, fp2, fn2, tp2 = conf_matrix
+    tn, fp, fn, tp, tn2, fp2, fn2, tp2 = C
     return acceptance_rate(tn2, fp2, fn2, tp2) - acceptance_rate(tn, fp, fn, tp)
 
 
-def equal_opportunity_difference(conf_matrix):
+def equal_opportunity_difference(C):
     """
     Compute the equal opportunity difference for the given confusion matrices.
-    :param conf_matrix: Confusion matrices of the privileged and unprivileged subgroup.
+
+    :param (int, int, int, int, int, int, int, int) C: Confusion matrices of the
+        privileged and unprivileged subgroup.
     :return: Equal opportunity difference.
+    :rtype: float
     """
-    _, _, fn, tp, _, _, fn2, tp2 = conf_matrix
-    return  recall(fn2, tp2) - recall(fn, tp)
+    _, _, fn, tp, _, _, fn2, tp2 = C
+    return recall(fn2, tp2) - recall(fn, tp)
 
 
-def average_odds_difference(conf_matrix):
+def average_odds_difference(C):
     """
     Compute the average odds difference for the given confusion matrices.
-    :param conf_matrix: Confusion matrices of the privileged and unprivileged subgroup.
+
+    :param (int, int, int, int, int, int, int, int) C: Confusion matrices of the
+        privileged and unprivileged subgroup.
     :return: Average odds difference.
+    :rtype: float
     """
-    tn, fp, fn, tp, tn2, fp2, fn2, tp2 = conf_matrix
+    tn, fp, fn, tp, tn2, fp2, fn2, tp2 = C
     fpr_diff = specificity(tn2, fp2) - specificity(tn, fp)
     tpr_diff = recall(fn2, tp2) - recall(fn, tp)
     return (tpr_diff + fpr_diff) / 2
@@ -41,11 +52,13 @@ def average_odds_difference(conf_matrix):
 def acceptance_rate(tn, fp, fn, tp):
     """
     Acceptance rate P / (P + N).
-    :param tn: True negatives.
-    :param fp: False positives.
-    :param fn: False negatives.
-    :param tp: True positives.
+
+    :param int tn: True negatives.
+    :param int fp: False positives.
+    :param int fn: False negatives.
+    :param int tp: True positives.
     :return: Acceptance rate.
+    :rtype: float
     """
     return (tp + fp) / (tp + fp + fn + tn)
 
@@ -53,9 +66,11 @@ def acceptance_rate(tn, fp, fn, tp):
 def recall(fn, tp):
     """
     Recall score tp / (tp + fn).
-    :param fn: False negatives.
-    :param tp: True positives.
+
+    :param int fn: False negatives.
+    :param int tp: True positives.
     :return: Recall score.
+    :rtype: float
     """
     if tp + fn > 0:
         return tp / (tp + fn)
@@ -65,9 +80,11 @@ def recall(fn, tp):
 def specificity(tn, fp):
     """
     Specificity score fp / (fp + tn)
-    :param tn: True negatives
-    :param fp: False positives.
+    
+    :param int tn: True negatives
+    :param int fp: False positives.
     :return: Specificity score.
+    :rtype: float
     """
     if fp + tn > 0:
         return fp / (fp + tn)
@@ -78,26 +95,47 @@ def compute_metrics(cluster_patterns, data, pos_label=1):
     """
     Print statistical parity, eq. opportunity and avg. odds difference for the given
     patterns of a clustering.
-    :param cluster_patterns: List of conjunctive patterns for each cluster
-    :param data: The original dataset (not preprocessed) incl. ground-truth ('class')
-    and predicted ('out') labels.
-    :param pos_label: Label of the positive class (0 or 1).
+    
+    :param dict of (list of (str, bool, any)) cluster_patterns: List of conjunctive 
+        patterns for each cluster
+    :param pd.DataFrame data: The original dataset (not preprocessed) 
+        incl. ground-truth ('class') and predicted ('out') labels.
+    :param int pos_label: Label of the positive class (0 or 1).
     :return: Dataframe with one row for each cluster (pattern)
+    :rtype: pd.DataFrame
     """
     res = pd.DataFrame(columns=['Stat. parity diff.', 'Eq. opportunity diff.', 'Avg. odds diff.'])
     for c in cluster_patterns:
-        patterns = cluster_patterns[c]
-        C = conf_matrix(data, patterns, pos_label=pos_label)
+        p = cluster_patterns[c]
+        C = conf_matrix(data, p, pos_label=pos_label)
         res.loc[c] = [statistical_parity_difference(C), equal_opportunity_difference(C), average_odds_difference(C)]
     return res
 
 
-def patterns_to_index(data, p):
+def subgroups_to_cluster_patterns(subgroups):
+    """
+    Transform the given entropy-based subgroups (patterns in form of a dataframe)
+    to a dictionary of conjunctive patterns (list of tuples).
+    
+    :param pd.DataFrame subgroups: Entropy-based subgroups in a DataFrame (each row is a subgroup)
+    :return: Dictionary of conjunctive patterns (list of (feature, op, value)-tuples)
+    :rtype: List of (str, bool, Any)
+    """
+    cluster_patterns = {}
+    for c, row in subgroups.iterrows():
+        p = [(fn, True, val) for fn, val in row.dropna().items()]
+        cluster_patterns[c] = p
+    return cluster_patterns
+
+
+def pattern_to_index(data, p):
     """
     Transform the patterns on the given dataset to a filter on the index (boolean array).
-    :param data: The original dataset (not preprocessed).
-    :param p: Conjunctive pattern (e.g., for a single cluster).
+    
+    :param pd.DataFrame data: The original dataset (not preprocessed).
+    :param list of (str, bool, Any) p: Conjunctive pattern (e.g., for a single cluster). 
     :return: Filter on the index of the dataset (boolean array).
+    :rtype: list of bool
     """
     conditions = []
     for pattern in p:
@@ -109,18 +147,20 @@ def patterns_to_index(data, p):
     return reduce(operator.and_, conditions)
 
 
-def conf_matrix(data, patterns, pos_label=1):
+def conf_matrix(data, p, pos_label=1):
     """
     Get the confusion matrices of the privileged and unprivileged subgroup.
-    :param data: Dataset with ground-truth and predicted labels
-    :param patterns: Patterns defining subgroup
-    :param pos_label: Label of the positive (favorable) class (0 or 1).
-    :return: tn, fp, fn, tp, tn2, fp2, fn2, tp2 (privileged & unprivileged)
+    
+    :param pd.DataFrame data: Dataset with ground-truth and predicted labels
+    :param list of (str, bool, Any) p: Conjunctive pattern defining a single subgroup
+    :param int pos_label: Label of the positive (favorable) class (0 or 1).
+    :return: tn, fp, fn, tp, tn2, fp2, fn2, tp2 (privileged & unprivileged subgroup)
+    :rtype: (int, int, int, int, int, int, int, int)
     """
     y_true = data['class']
     y_pred = data['out']
 
-    idx = patterns_to_index(data, patterns)
+    idx = pattern_to_index(data, p)
     y_true_group = y_true[idx]
     y_pred_group = y_pred[idx]
 
@@ -138,34 +178,144 @@ def conf_matrix(data, patterns, pos_label=1):
 
 def pattern_support(p, data):
     """
-    Compute the support of a given conjunctive pattern.
-    :param p: Conjunctive pattern of the form (col T/F val) ^ (col2 T/F val2) ^ ...
-    where operators are = or !=.
-    :type p: List of (str, bool, Any)
-    :param data: The original dataset (not preprocessed).
-    :type data: pd.DataFrame
+    Compute the support of a given conjunctive pattern, i.e., the ratio
+    of instances that satisfy the pattern.
+    
+    :param List of (str, bool, Any) p: Conjunctive pattern of the form 
+        '(col op val) ^ (col2 op val2) ^ ...' where operators in the form of
+        booleans are '=' (True) or '!=' (False). The conjunctive pattern is
+        presented as a list of the individual attribute-value pairs.
+    :param pd.DataFrame data: The original dataset (not preprocessed).
     :return: Support of the conjunctive pattern
     :rtype: float
     """
-    idx = patterns_to_index(data, p)
+    idx = pattern_to_index(data, p)
     return idx.sum() / len(data)
 
 
-def containment_score(p, p_set, data):
-    idx = patterns_to_index(data, p)
+def cluster_pattern_support(cluster_patterns, data):
+    """
+    Compute the support of each pattern in the dataset.
+    
+    :param dict of (list of (str, bool, any)) cluster_patterns: Dict of conjunctive
+        patterns for each cluster.
+    :param pd.DataFrame data: The original dataset (not preprocessed).
+    :return: Support per pattern
+    :rtype: dict of float
+    """
+    support = {}
+    for c, p in cluster_patterns.items():
+        support[c] = pattern_support(p, data)
+    return support
 
+
+def containment_score(p, p_list, data):
+    """
+    Compute the containment score of the given pattern in comparison to the
+    patterns from the list.
+
+    :param list of (str, bool, Any) p: Single conjunctive pattern.
+    :param list of (list of (str, bool, Any)) p_list: List of conjunctive patterns.
+    :param pd.DataFrame data: The original dataset (not preprocessed).
+    :return: Containment score of p compared to all patterns in p_list
+    :rtype: float
+    """
+    idx = pattern_to_index(data, p)
     scores = []
-    for p2 in p_set:
-        idx2 = patterns_to_index(data, p2)
+    for p2 in p_list:
+        idx2 = pattern_to_index(data, p2)
         scores.append((idx & idx2).sum() / idx.sum())
-
     return np.max(scores)
 
 
-def cluster_containment_scores(cluster_patterns, data):
-    scores = {}
+def cluster_containment_score(cluster_patterns, data):
+    """
+    Compute the containment score for conjunctive patterns in a one-vs-rest fashion,
+    i.e., compute the containment of each of the patterns in comparison to all other
+    patterns except the selected.
+
+    :param dict of (int, list of (str, bool, Any)) cluster_patterns: Dict of conjunctive
+        patterns for each cluster.
+    :param pd.DataFrame data: The original dataset (not preprocessed).
+    :return: Containment score per pattern
+    :rtype: dict of float
+    """
+    containment = {}
     for c, p in cluster_patterns.items():
         other_patterns = cluster_patterns.copy()
         other_patterns.pop(c)
-        scores[c] = containment_score(p, other_patterns.values(), data)
-    return scores
+        containment[c] = containment_score(p, other_patterns.values(), data)
+    return containment
+
+
+def cluster_pattern_size(cluster_patterns):
+    """Compute the pattern sizes of the given patterns.
+
+    :param dict of (list of (str, bool, Any)) cluster_patterns: Dict of conjunctive
+        patterns for each cluster.
+    :return: Sizes of the patterns
+    :rtype: dict of float
+    """
+    return _dict_map(cluster_patterns, len)
+
+
+def average_pattern_size(cluster_patterns):
+    """Compute the average pattern size over the given patterns.
+
+    :param dict of (list of (str, bool, Any)) cluster_patterns: Dict of conjunctive
+        patterns for each cluster.
+    :return: Average pattern size
+    :rtype: float
+    """
+    return _dict_avg(cluster_pattern_size(cluster_patterns))
+
+
+def fidelity(p, c, data, labels):
+    """Compute the fidelity of the given pattern on the given dataset.
+
+    :param list of (str, bool, Any) p: Single conjunctive pattern
+    :param int c: Cluster number
+    :param pd.DataFrame data: The original dataset (not preprocessed).
+    :param list of int labels: Cluster labels
+    :return: Fidelity of p
+    :rtype: float
+    """
+    idx = pattern_to_index(data, p)
+    cluster_idx = labels == c
+    return (idx & cluster_idx).sum() / idx.sum()
+
+
+def cluster_fidelity(cluster_patterns, data, labels):
+    """Compute the fidelity of the given dict of conjunctive patterns for each cluster.
+
+    :param dict of (list of (str, bool, Any)) cluster_patterns: Dict of conjunctive
+        patterns for each cluster.
+    :param pd.DataFrame data: The original dataset (not preprocessed).
+    :param list of int labels: Cluster labels
+    :return: Fidelity of each pattern
+    :rtype: dict of float
+    """
+    # Compute the fidelity for each pattern/cluster
+    fidelities = {}
+    for c, p in cluster_patterns.items():
+        fidelities[c] = fidelity(p, c, data, labels)
+    return fidelities
+
+
+def cluster_coverage(cluster_patterns, data):
+    idx = [False] * len(data)
+    for c, p in cluster_patterns.items():
+        idx = idx | pattern_to_index(data, p)       # concatenate all index lists with logical or
+    return idx.sum() / len(data)        # ratio of instances affected by a pattern
+
+
+def print_pattern_metrics(cluster_patterns, data, labels):
+    size = cluster_pattern_size(cluster_patterns)
+    print(f"Pattern size:\t\t{_dict_avg(size)} ({_dict_min(size)} - {_dict_max(size)})")
+    support = cluster_pattern_support(cluster_patterns, data)
+    print(f"Support:\t\t{_dict_avg(support)} ({_dict_min(support)} - {_dict_max(support)})")
+    print(f"Coverage:\t\t{cluster_coverage(cluster_patterns, data)}")
+    containment = cluster_containment_score(cluster_patterns, data)
+    print(f"Containment scores:\t{_dict_avg(containment)} ({_dict_min(containment)} - {_dict_max(containment)})")
+    fidelity = cluster_fidelity(cluster_patterns, data, labels)
+    print(f"Fidelity: \t\t{_dict_avg(fidelity)} ({_dict_min(fidelity)} - {_dict_max(fidelity)})")
--- subgroup_detection/util.py
+++ subgroup_detection/util.py
@@ -1,16 +1,14 @@
+import numpy as np
 import pandas as pd
 
 def label_data(data, labels, remove_outliers=False):
-    """
-    Label the given dataset with clustering labels.
-    @param data: Dataset
-    @type data: pd.DataFrame
-    @param labels: Clustering labels (cluster numbers)
-    @type labels: list
-    @param remove_outliers: If true, remove the outliers (data points labeled -1)
-    @type remove_outliers: bool
-    @return: Labeled dataset
-    @rtype: pd.DataFrame
+    """Label the given dataset with clustering labels.
+
+    :param pd.DataFrame data: Dataset
+    :param list of int labels: Clustering labels (cluster numbers)
+    :param bool remove_outliers: If true, remove the outliers (data points labeled -1)
+    :return: Labeled dataset
+    :rtype: pd.DataFrame
     """
     labeled = data.drop(labels=['out', 'class'], axis=1)
     labeled['cluster'] = labels
@@ -20,15 +18,14 @@ def label_data(data, labels, remove_outliers=False):
 
 
 def _scale(data, exclude=[]):
-    """
-    Min-max-scale the given dataset inplace.
+    """Min-max-scale the given dataset inplace.
+
     Note: All columns have to be numeric.
-    @param data: Dataset
-    @type data: pd.DataFrame
-    @param exclude: Names of columns to exclude from scaling
-    @type exclude: list of str
-    @return: Scaled dataset
-    @rtype: pd.Dataframe
+
+    :param pd.DataFrame data: Dataset
+    :param list of str exclude: Names of columns to exclude from scaling
+    :return: Scaled dataset
+    :rtype: pd.Dataframe
     """
     if exclude:
         dc = data.copy()
@@ -41,29 +38,75 @@ def _scale(data, exclude=[]):
 
 
 def prepare(data, categ_columns=None, label_column='class', prediction_column='out', prefix_sep='#', **kwargs):
-    """
-    Transform a given dataset into a numeric dataset by
+    """Transform a given dataset into a numeric dataset by
         - removing the label and prediction column,
         - converting categorical variables into indicators,
         - and min-max-scaling.
-    @param data: Dataset
-    @type data: pd.DataFrame
-    @param kwargs: Keyword arguments to pass to the scaling method
-    @type kwargs: Keyword arguments
-    @param categ_columns: List of categorical columns or None. If None, then all columns
-    with type 'category' or 'object' are encoded
-    @type categ_columns: None or list of str
-    @param label_column: Name of column with ground-truth class labels
-    @type label_column: str
-    @param prediction_column: Name of column with predicted class labels
-    @type prediction_column: str
-    @param prefix_sep: Delimiter to use for one-hot encoding, e.g., sex = {M, F} --> sex#M = {0, 1}.
-    Should be a character or string that does not appear in any of the feature names of the dataset.
-    @type prefix_sep: str
-    @return: Transformed numeric dataset
-    @rtype: pd.DataFrame
+        
+    :param pd.DataFrame data: Dataset
+    :param kwargs: Keyword arguments to pass to the scaling method
+    :param  None or list of str categ_columns: List of categorical columns or None. 
+        If None, then all columns with type 'category' or 'object' are encoded
+    :param str label_column: Name of column with ground-truth class labels
+    :param str prediction_column: Name of column with predicted class labels
+    :param str prefix_sep: Delimiter to use for one-hot encoding, e.g., sex = {M, F} --> sex#M = {0, 1}.
+        Should be a character or string that does not appear in any of the feature names of the dataset.
+    :return: Transformed numeric dataset
+    :rtype: pd.DataFrame
     """
     data_num = data.copy().drop(labels=[label_column, prediction_column], axis=1)
     data_num = pd.get_dummies(data_num, drop_first=True, columns=categ_columns, prefix_sep=prefix_sep)  # One-hot enc.
     data_num = _scale(data_num, **kwargs)  # Min-max scaling
     return data_num
+
+
+def _dict_min(d):
+    """Minimum value of dictionary values.
+
+    :param dict of int or dict of float d: Dictionary with numeric values.
+    :return: Minimum value of the dictionary values.
+    :rtype: int or float
+    """
+    return _dict_apply(d, np.min)
+
+
+def _dict_max(d):
+    """Maximum value of dictionary values.
+
+    :param dict of int or dict of float d: Dictionary with numeric values.
+    :return: Maximum value of the dictionary values.
+    :rtype: int or float
+    """
+    return _dict_apply(d, np.max)
+
+
+def _dict_avg(d):
+    """Average value of dictionary values.
+
+    :param dict of int or dict of float d: Dictionary with numeric values.
+    :return: Average value of the dictionary values.
+    :rtype: int or float
+    """
+    return _dict_apply(d, np.mean)
+
+
+def _dict_apply(d, f):
+    """Apply a function to the list of values of a dictionary.
+
+    :param dict d: Dictionary.
+    :param Callable f: Function.
+    :return: Result of the application of f on the values of d.
+    :rtype: Any
+    """
+    return f(list(d.values()))
+
+
+def _dict_map(d, f):
+    """Map the values of a dictionary using a function.
+
+    :param dict d: Dictionary.
+    :param Callable f: Function.
+    :return: Dictionary with same keys and results of f(d[key])
+    :rtype: dict
+    """
+    return {k: f(v) for k, v in d.items()}
